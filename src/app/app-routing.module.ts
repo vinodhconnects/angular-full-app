af7ea4e11e929611ec7d1e2112f2c29fd4168dae
vinodhconnects <vinodhconnects@gmail.com>
2023-06-28 16:03:43 +0530

    basic-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ReviewsComponent } from './pages/reviews/reviews.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component: HomeComponent},
+  {path:"projects",component: ProjectsComponent},
+  {path:"contacts",component:ContactsComponent},
+  {path:"reviews",component: ReviewsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
